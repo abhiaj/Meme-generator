cc469e7104785486c9cd36c88725ef68c6ad96fe
abhiaj <abhishekburnwal2@gmail.com>
2019-05-29 20:18:15 +0530

    added backend and display part

---
+++ Header.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+class Header extends React.Component{
+
+	constructor(){
+		super()
+		this.state={}
+	}
+	
+	render(){
+		return(
+			<h2> alpha</h2>
+			)
+	}
+}
+
+export default Header
---
+++ MemeGenerator.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+class MemeGenerator extends React.Component
+{
+	constructor(){
+		super()
+		this.state={}
+	}
+
+	render(){
+		return(
+			<div>
+				<h4>yoho</h4>
+			</div>
+		)
+	}
+}
+
+export default MemeGenerator
